8c34a06d4fbea1976c9190ce45ce6a5f8e22bc56
Brittany Nicole <bnsquires1@gmail.com>
2024-05-08 22:39:01 -0400

    push

--- object-oriented-adventure/script.js
+++ object-oriented-adventure/script.js
@@ -32,3 +32,48 @@ console.log("===========")
 for (let i = 0; i < adventurer.inventory.length; i++) {
     console.log(adventurer.inventory[i])
 };
+
+class Character {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+};
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+// Character.roll(); err: "character.roll is not a function" 
+
+///////// pt 3
+
+class Adventurer extends Character {
+    constructor(name, role) {
+        super(name);
+        // Adventurers have specialized roles.
+        this.role = role;
+        // Every adventurer starts with a bed and 50 gold coins.
+        this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout() {
+        console.log(`${this.name} is scouting ahead...`);
+        super.roll();
+    }
+    /// Adventurers should have the ability to run
+    run() {
+        console.log(`${this.name} is running ahead...`);
+        super.roll();
+    }
+        /// Adventurers should have the ability to jump
+        jump () {
+            console.log(`${this.name} is jumping high!...`);
+            super.roll();
+        }
+}
